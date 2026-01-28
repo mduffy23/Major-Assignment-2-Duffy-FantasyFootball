5f61682ebdbc8d87321c3d53051537b92979a9c8
Matthew Duffy <mduffy23@hotmail.com>
2026-01-27 20:16:17 -0500

    adding Reflection, ToDo, multiple file data ability, and analysis

--- major_assignment_2_duffy/dataset.py
+++ major_assignment_2_duffy/dataset.py
@@ -5,20 +5,24 @@ import typer
 import pandas as pd
 
 
-from major_assignment_2_duffy.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, FIGURES_DIR
+from major_assignment_2_duffy.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 app = typer.Typer()
 
 
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
+    input_dir: Path = RAW_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
-    # To process multiple I could add a for loop, but the cleaning would be meaningless unless they all needed to be cleaned in the same way (which is simply unlikely)
-    ## For that reason, we will rock with one file
-    df = pd.read_csv(input_path)
+    # Get all csv files
+    dfs = []
+    for csv_path in input_dir.glob("*.csv"):
+        df = pd.read_csv(csv_path)
+        dfs.append(df)
+
+    df = pd.concat(dfs, ignore_index=True)
     df.drop(columns=['player_id'], inplace=True)
     df.to_csv(output_path, index=False)
 if __name__ == "__main__":
